78c8f3a1b030ce67c650e7aef02849a080800698
Warren MacEvoy <wmacevoy@gmail.com>
2018-10-02 12:18:48 -0600

    threads start

---
+++ threads/src/test/java/threads/ListSpeedTestTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wmacevoy
+ */
+public class ListSpeedTestTest {
+    
+    public ListSpeedTestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testCollectionsSortSpeed() {
+        ListSpeedTest test =  new ListSpeedTest();
+        test.setAction(new ListSorter());
+        test.start();
+        test.waitTilFinish();
+    }
+    
+}
